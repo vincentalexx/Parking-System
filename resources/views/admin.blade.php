cdf7f77aaf7c45137cbc007bf6f263783ab0ae52
vincent vincent <vincentalexx03@gmail.com>
2024-02-18 00:48:04 +0700

    minor styling

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -4,27 +4,49 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Admin</title>
+    <title>Admin - Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <style>
+        body {
+            height: 100vh;
+            background-color: black;
+            font-family: 'Courier New', Courier, monospace
+        }
+
+        .login {
+            background-color: black;
+            color: white;
+            border: none;
+            padding: 5px 10px;
+            border-radius: 5px;
+            cursor: pointer;
+            font-weight: bold
+        }
+        .login:hover {
+            background-color: white;
+            color: black;
+            transition: all 0.5s ease-out;
+        }
+    </style>
 </head>
-<body>
-    <form method="POST" action="{{route('admin.login.store')}}">
+<body class="d-flex justify-content-center align-items-center">
+    <div class="container h-50 d-flex justify-content-center align-items-center" style="background-color: rgb(36, 36, 36); border-radius: 5%; width: 500px">
+        <form method="POST" action="{{route('admin.login.store')}}">
         @csrf
-        <div>
-            <div>
-                <p>LOGIN ADMIN</p>
-                <div>
-                    <label for="email">Email</label>
-                    <input type="text" name="email" placeholder="example@email.com">
-                </div>
-                <div>
-                    <label for="password">Password</label>
-                    <input type="password" name="password">
-                </div>
-                <p style="color: red">{{ Session::pull('authError') }}</p>
-                <button>Login</button>
-                <a href="{{route('user.signup')}}">Sign Up</a>
+            <p class="text-light text-center text-start fs-3 fw-bold"> ADMIN LOGIN</p>
+            <div class="d-flex mb-2 justify-content-center ">
+                <label class="text-light w-25 fw-semibold" for="email">Email</label>
+                <input class="px-2 rounded border-0 ms-3" type="text" name="email" placeholder="example@email.com">
             </div>
-        </div>
-    </form>
+            <div class="d-flex mb-2 justify-content-center ">
+                <label class="text-light w-50 fw-semibold" for="password">Password</label>
+                <input class="px-2 rounded border-0 ms-3" type="password" name="password">
+            </div>
+            <p style="color: red">{{ Session::pull('authError') }}</p>
+            <div class="d-flex flex-column mt-2">
+                <button class="login">Login</button>
+            </div>
+        </form>
+    </div>
 </body>
 </html>
--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Parking System</title>
+    <title>Parking System - Sign Up</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <style>
     body {
@@ -40,33 +40,33 @@
     </style>
 </head>
 <body class="d-flex justify-content-center align-items-center">
-    <div class="container d-flex justify-content-center align-items-center" style="background-color: rgb(36, 36, 36); border-radius: 5%; width: 500px; height: 60%;">
+    <div class="container d-flex justify-content-center align-items-center" style="background-color: rgb(36, 36, 36); border-radius: 5%; width: 600px; height: 60%;">
         <form method="POST" action="{{route('user.signup.store')}} " class="flex justify-content-center align-items-center">
         @csrf
             <p class="text-light text-center text-start fs-3 fw-bold">Sign Up</p>
             <div class="d-flex mb-2 justify-content-center">
-                <label class="text-light text-left w-50 fw-semibold" for="name">Name</label>
+                <label class="text-light w-50 fw-semibold" for="name">Name</label>
                 <input class="px-2 rounded border-0 ms-3" type="text" name="name" placeholder="Your Name">
             </div>
             @error('name')
                 <div style="color: red">{{ $message }}</div>
             @enderror
             <div class="d-flex mb-2 mt-2 justify-content-center">
-                <label class="text-light text-left w-50 fw-semibold" for="email">Email</label>
+                <label class="text-light w-50 fw-semibold" for="email">Email</label>
                 <input class="px-2 rounded border-0 ms-3" type="text" name="email" placeholder="example@email.com">
             </div>
             @error('email')
                 <div style="color: red">{{ $message }}</div>
             @enderror
             <div class="d-flex mb-2 mt-2 justify-content-center">
-                <label class="text-light text-left w-50 fw-semibold" for="phone">Phone Number</label>
+                <label class="text-light w-50 fw-semibold" for="phone">Phone Number</label>
                 <input class="px-2 rounded border-0 ms-3" type="text" name="phone" placeholder="123456789">
             </div>
             @error('phone')
                 <div style="color: red">{{ $message }}</div>
             @enderror
             <div class="d-flex mb-2 mt-2 justify-content-center">
-                <label class="text-light text-left w-50 fw-semibold" for="password">Password</label>
+                <label class="text-light w-50 fw-semibold" for="password">Password</label>
                 <input class="px-2 rounded border-0 ms-3" type="password" name="password">
             </div>
             @error('password')
